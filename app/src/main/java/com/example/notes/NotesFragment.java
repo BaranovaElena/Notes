c5424e40ffc18300708b19ee6ca3c0369310597d
BaranovaElena <baranovaeaa@yandex.ru>
2021-06-06 21:03:24 +0300

    украшательства интерфейса

--- app/src/main/java/com/example/notes/NotesFragment.java
+++ app/src/main/java/com/example/notes/NotesFragment.java
@@ -3,6 +3,7 @@ package com.example.notes;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.text.Html;
@@ -10,9 +11,9 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class NotesFragment extends Fragment {
     private final NoteEntity[] notes = {
@@ -23,8 +24,6 @@ public class NotesFragment extends Fragment {
             new NoteEntity("Note5", "fifth note", "24.05.2021", "my fifth note")};
 
     private LinearLayout layoutNotesList;
-
-    NotesFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,15 +46,24 @@ public class NotesFragment extends Fragment {
 
     private void initNotesList() {
         for (NoteEntity note : notes) {
-            TextView textView = new TextView(getContext());
+            MaterialButton button = new MaterialButton(requireContext());
 
-            textView.setText(Html.fromHtml("<b>"+note.getTitle()+"</b>\t("+note.getDate()+")"));
-            textView.setTextSize(getResources().getDimension(R.dimen.note_list_text_size));
-            textView.setGravity(Gravity.CENTER);
-            textView.setOnClickListener(v ->
+            setButtonProperties(button);
+            button.setText(Html.fromHtml("<b>"+note.getTitle()+"</b>\t("+note.getDate()+")"));
+            button.setOnClickListener(v ->
                 ((Controller) requireActivity()).openNoteScreen(note));
-            layoutNotesList.addView(textView);
+
+            layoutNotesList.addView(button);
         }
+    }
+
+    private void setButtonProperties(MaterialButton button) {
+        button.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.background));
+        button.setStrokeWidthResource(R.dimen.button_stroke_width);
+        button.setStrokeColorResource(R.color.button_stroke);
+        button.setTextSize(getResources().getDimension(R.dimen.note_list_text_size));
+        button.setTextColor(ContextCompat.getColor(requireContext(), R.color.button_text));
+        button.setGravity(Gravity.CENTER);
     }
 
     interface Controller {
